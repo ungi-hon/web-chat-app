8f36adb8320d6d83168d59810e71bded2c5b84b7
ugngi <50014619+ungi-hon@users.noreply.github.com>
2020-09-20 02:02:42 +0900

    feat: store

--- store/index.ts
+++ store/index.ts
@@ -1,48 +1,75 @@
-import { reactive } from 'nuxt-composition-api'
+import { reactive, useContext } from 'nuxt-composition-api'
 import { GetterTree, MutationTree, ActionTree } from 'vuex'
 import { User } from 'firebase'
+import createPersistedState from 'vuex-persistedstate'
 import { RootState, AutoState } from '~/store/types'
+
 import firebase from '~/plugins/firebase'
 
-export const autoState = () => {
+export const state = () => {
   return reactive<AutoState>({
-    userUid: '',
+    isLogin: false,
     userName: '',
   })
 }
 
 export const mutations: MutationTree<AutoState> = {
-  setUserUid(autoState, userUid) {
-    autoState.userUid = userUid
+  setIsLogin(state, isLogin) {
+    state.isLogin = isLogin
   },
-  setUserName(autoState, userName) {
-    autoState.userName = userName
+  setUserUid(state, userUid) {
+    state.userUid = userUid
+  },
+  setUserName(state, userName) {
+    state.userName = userName
   },
 }
 
 export const actions: ActionTree<AutoState, RootState> = {
-  login({ commit }) {
+  nuxtClientInit() {
+    createPersistedState()(this)
+  },
+  // emailLogin({ commit }, { email, password, name }) {
+  //   firebase
+  //     .auth()
+  //     .signInWithEmailAndPassword(email, password)
+  //     .then((result) => {
+  //       const user: User | null = result.user
+  //       user?.updateProfile({
+  //         displayName: name,
+  //       })
+  //       commit('setUserName', user?.displayName)
+  //       commit('setIsLogin', true)
+  //     })
+  //     .catch((error) => {
+  //       alert(error)
+  //     })
+  // },
+  googleLogin({ commit }) {
     const provider = new firebase.auth.GoogleAuthProvider()
     firebase
       .auth()
       .signInWithPopup(provider)
       .then((result) => {
         const user: User | null = result.user
-        commit('setUserUid', user?.uid)
         commit('setUserName', user?.displayName)
+        commit('setIsLogin', true)
       })
       .catch((error) => {
         const errorCode = error.code
         console.log('error : ' + errorCode)
       })
   },
+  setUserName({ commit }, displayName) {
+    commit('setUserName', displayName)
+  },
+  setIsLogin({ commit }) {
+    commit('setIsLogin', true)
+  },
 }
 
 export const getters: GetterTree<AutoState, RootState> = {
-  getUserUid(autoState) {
-    return autoState.userUid
-  },
-  getUserName(autoState) {
-    return autoState.userName
+  getUserName(state) {
+    return state.userName
   },
 }
